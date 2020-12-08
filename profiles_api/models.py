9afe5783088f4b332a2126e41e55f9239eb1315b
Rakibul Islam <rislam89@outlook.com>
2020-12-08 22:15:26 +0100

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,51 +5,51 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-    """ Manager for user Profiles"""
+    """Manager for user profiles"""
 
-    def create_user(self, email, name, password= None):
-        """Create a new user Profile"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)   # create a model object for email and name
+        user = self.model(email=email, name=name,)
 
-        user.set_password(password) # make the password encripted
-
+        user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
     def create_superuser(self, email, name, password):
-        """Create and save a new Superuser with given details"""
-        user = self.create_user(email, name, password=True) # creating user using create_user function
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """"Database model for user in the system"""
+    """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True) #active users can log in
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    """
-    Okay finally we need to specify the string representation of our model now this is the item
-    that we want to return when we convert a user profile object to a string in
-    Python you do that by typing
-    """
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
+
     def get_short_name(self):
-        """Retrive short name of user"""
+        """Retrieve short name of user"""
         return self.name
-    def ___str___(self):
-        """Return string representation of our user"""
+
+    def __str__(self):
+        """Return string representation of user"""
         return self.email
